d28c243324da2ba2b2061a11a0a0c59d0638aacf
Uzairkhan7 <m.uzairkhan7@gmail.com>
2020-03-30 15:58:41 +0500

    added more functionality

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 //sign up
 let allUsers
-let user = {}
 let userID = Math.floor(Math.random()*10000)
+let catID = Math.floor(Math.random()*10000)
+let expID = Math.floor(Math.random()*10000)
 
 
-
-document.getElementById("signup").onclick=()=>{
+function signup(){
     
     let userName = document.getElementById("usr").value;
     let email = document.getElementById("email").value;
     let pasword = document.getElementById("pwd").value;
     
-     user = {
+    let user = {
         userName,
         email,
         pasword,
@@ -58,7 +58,7 @@ function signIn(){
 
                  //signin authentication
 
-   allUsers.map((val)=>{
+   allUsers.filter((val)=>{
        if(val.email === sEmail && val.pasword === sPwd){
            window.location = "./main.html"
            console.log("pasword and email matched !");
@@ -71,23 +71,143 @@ function signIn(){
 //add category
 
 function addCat(){
-    let cat = document.getElementById('cat').value;
-    let catArr = 
+    let category = document.getElementById('cat').value;
+    let catArr = {
+        catID,
+        category
+    }
+    if(localStorage.getItem("allCategories")){
+        let allCategories = JSON.parse(localStorage.getItem("allCategories")) 
+        allCategories.push(catArr)
+        localStorage.setItem("allCategories", JSON.stringify(allCategories))
+    }else{
+        let allCategories = []
+        allCategories.push(catArr)
+        localStorage.setItem("allCategories", JSON.stringify(allCategories))
+    }
+    console.log("category added !");
     
     
 }
 
 
-// let arr = []
-// let d = new Date()
-// let date = d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear()
 
-// document.getElementById('addExp').onclick =()=>{
 
-//     let des = document.getElementById('des').value;
-//     let amount = document.getElementById('amount').value;
-//     let cat = document.getElementById('cat').value;
-//     arr.push(des, amount, date)
-//     localStorage.setItem(cat,JSON.stringify(arr))
-//     console.log('Data saved!')
-// }
+
+                    //add Expenses
+function addExpenses(){
+    let d = new Date()
+    let month = d.getMonth()+1
+    let date = d.getDate() + "/" + month + "/" + d.getFullYear()
+    
+    let des = document.getElementById('des').value;
+    let amount = document.getElementById('amount').value;
+    let cat = document.getElementById('cat').value;
+
+    
+
+    let expense = {
+        expID,
+        des,
+        amount,
+        cat,
+        date
+        
+    }
+    
+    console.log('expense: ', expense);
+
+   if(localStorage.getItem("allExpenses")){
+    let allExpenses =  JSON.parse(localStorage.getItem("allExpenses"))
+     allExpenses.push(expense)
+     localStorage.setItem("allExpenses", JSON.stringify(allExpenses))
+   }else{
+       let allExpenses = []
+      allExpenses.push(expense)
+      console.log('allExpenses: ', allExpenses);
+      localStorage.setItem("allExpenses", JSON.stringify(allExpenses) )
+   }
+   
+
+    alert("Expenses Added !!")
+    location.reload()
+
+}
+
+                //////   Logout ////
+
+function logout(){
+    localStorage.removeItem("currentUser")
+    window.location = "./index.html"
+
+}
+
+
+/* <th class="table-data"> ID</th>
+<th class="table-data"> Description</th>
+<th class="table-data"> Amount</th>
+<th class="table-data"> Category</th>
+<th class="table-data"> Created On</th>
+<th class="text-center ">
+    <button type="button" class="btn btn-success ">Edit</button>
+    <button type="button" class="btn btn-danger ">Delete</button>    
+</th> */
+function renderTable(){
+    let table = document.getElementById("tableData")
+    let allExpenses = JSON.parse(localStorage.getItem("allExpenses"))
+
+    for(let i=0; i<allExpenses.length; i++){
+    let tr = document.createElement("tr")
+    
+    let id_th = document.createElement("th")
+        id_th.setAttribute("class", "table-data")
+        id_th.innerHTML = allExpenses[i].expID
+        tr.appendChild(id_th)
+
+    let des_th = document.createElement("th")
+        des_th.setAttribute("class", "table-data")
+        des_th.innerHTML = allExpenses[i].des
+        tr.appendChild(des_th)
+
+    let amount_th = document.createElement("th")
+        amount_th.setAttribute("class", "table-data")
+        amount_th.innerHTML = allExpenses[i].amount
+        tr.appendChild(amount_th)   
+        
+    let cat_th = document.createElement("th")
+        cat_th.setAttribute("class", "table-data")
+        cat_th.innerHTML = allExpenses[i].cat
+        tr.appendChild(cat_th) 
+    
+    let created_th = document.createElement("th")
+        created_th.setAttribute("class", "table-data") 
+        created_th.innerHTML = allExpenses[i].date
+        tr.appendChild(created_th)
+
+     let btn_th = document.createElement("th")
+          btn_th.setAttribute("class", "text-center") 
+     
+     let edit_btn = document.createElement("button")
+         edit_btn.setAttribute("type", "button")
+         edit_btn.setAttribute("class", "btn btn-success")
+         edit_btn.innerHTML = "Edit"
+
+         delete_btn = document.createElement("button")
+         delete_btn.setAttribute("type", "button")
+         delete_btn.setAttribute("class", "btn btn-danger")
+         delete_btn.innerHTML = "Delete"
+
+
+         btn_th.appendChild(edit_btn)
+         btn_th.appendChild(delete_btn)
+        
+        tr.appendChild(btn_th)
+
+        table.appendChild(tr)
+    }
+
+
+    console.log("loop")
+
+}
+
